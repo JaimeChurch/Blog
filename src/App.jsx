57c805f69cddd9f58ba283fb18c568281fc4c332
Jaime Church <deesjaime@gmail.com>
2025-01-21 14:43:58 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>React Blog</h1>
+      <p>
+        <h3>Blog Post 1</h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
+      </p>
+
+      <p>
+        <h3>Blog Post 2</h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
+      </p>
+
+      <p>
+        <h3>Blog Post 3</h3>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
+      </p>
+
+    </>
+  )
+}
+
+export default App
+
+
