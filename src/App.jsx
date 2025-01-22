a2ac9482e742832afb24eb6c72c9b9bb87e5dd5f
Jaime Church <deesjaime@gmail.com>
2025-01-22 13:53:09 -0800

    Added paper element for displaying blog posts.

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,62 @@
-import './App.css'
+import React from 'react';
+import { Box, Paper } from '@mui/material';
+import './App.css';
 
-function App() {
+function SimplePaper({ children }) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 1,
+        margin: 1,
+      }}
+    >
+      <Paper elevation={2} sx={{ padding: 2 }}>
+        {children}
+      </Paper>
+    </Box>
+  );
+}
 
+export default function App() {
   return (
     <>
       <h1>React Blog</h1>
-      <p>
-        <h3>Blog Post 1</h3>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
-      </p>
+      <SimplePaper>
+        <h3>To Do:</h3>
+        <ul>
+          <li>Home button</li>
+          <li>Add note card styling</li>
+          <li>Import questions</li>
+          <li>Make answers clickable</li>
+          <li>Flip animation</li>
+          <li>Add topic selection</li>
+          <li>Add high score table</li>
+        </ul>
+      </SimplePaper>
 
-      <p>
-        <h3>Blog Post 2</h3>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
-      </p>
+      <SimplePaper>
+        <h3>Week 1</h3>
+        <ul>
+          <li>Started new PMBOK flashcard app.</li>
+        </ul>
+      </SimplePaper>
 
-      <p>
-        <h3>Blog Post 3</h3>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo est inventore animi culpa quasi, laudantium nisi magnam quos ipsa placeat mollitia aliquam tempora sed voluptas itaque consectetur explicabo accusantium laboriosam.
-      </p>
+      <SimplePaper>
+        <h3>Week 2</h3>
+        <ul>
+          <li>Started new PMBOK flashcard app.</li>
+        </ul>
+      </SimplePaper>
 
+      <SimplePaper>
+        <h3>Week 2</h3>
+        <ul>
+          <li>Started new PMBOK flashcard app.</li>
+        </ul>
+      </SimplePaper>
     </>
-  )
+  );
 }
-
-export default App
-
-
